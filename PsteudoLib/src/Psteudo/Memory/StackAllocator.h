cdf2d03eb456a2925773797e3a2e1fecc37d3d35
Stuart Smith <smith.stuart.t@gmail.com>
2019-08-27 22:53:08 -0800

    test

--- PsteudoLib/src/Psteudo/Memory/StackAllocator.h
+++ PsteudoLib/src/Psteudo/Memory/StackAllocator.h
@@ -11,12 +11,17 @@ namespace psteudo {
 			//don't want to type std::uint32_t all the time
 			typedef std::uint32_t U32;
 
+			//constructs a stack allocator of specified size
 			explicit StackAllocator(U32 stackSize_bytes);
 
+			//allocates the the specified amount of memory from the top of the stack
 			void* alloc(U32 size_bytes);
 
+			//returns the current position in the stack
 			Marker getMarker();
 
+			//frees memory back to the selected marker
+			//
 			void freeToMarker(Marker marker);
 
 			void clear();
